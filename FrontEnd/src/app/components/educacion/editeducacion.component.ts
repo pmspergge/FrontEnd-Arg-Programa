9d25167a9d18638cead92b25ee9216aab3f8f100
Matias Spergge <pmatiasspergge@gmail.com>
2022-12-15 12:24:41 -0300

    update app routing module

--- FrontEnd/src/app/components/educacion/editeducacion.component.ts
+++ FrontEnd/src/app/components/educacion/editeducacion.component.ts
@@ -6,38 +6,40 @@ import { EducacionService } from 'src/app/service/educacion.service';
 @Component({
   selector: 'app-editeducacion',
   templateUrl: './editeducacion.component.html',
-  styleUrls: ['./editeducacion.component.css']
+  styleUrls: ['./editeducacion.component.css'],
 })
 export class EditeducacionComponent implements OnInit {
-  educacion: Educacion = null;
-  
+  educacion: Educacion = null!;
+
   constructor(
     private educacionS: EducacionService,
-    private activatedRouter : ActivatedRoute,
+    private activatedRouter: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
     this.educacionS.detail(id).subscribe(
-      data =>{
+      (data) => {
         this.educacion = data;
-      }, err =>{
-         alert("Error al modificar");
-         this.router.navigate(['']);
+      },
+      (err) => {
+        alert('Error al modificar');
+        this.router.navigate(['']);
       }
-    )
+    );
   }
 
-  onUpdate(): void{
+  onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
     this.educacionS.update(id, this.educacion).subscribe(
-      data => {
+      (data) => {
         this.router.navigate(['']);
-      }, err => {
-        alert("Error al modificar la educacion");
+      },
+      (err) => {
+        alert('Error al modificar la educacion');
         this.router.navigate(['']);
       }
-    )
+    );
   }
 }
