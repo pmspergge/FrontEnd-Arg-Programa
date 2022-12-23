d1b7bc2562d70b7452b51ee98319a0f22e02a5aa
Matias Spergge <pmatiasspergge@gmail.com>
2022-12-23 16:53:34 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL: 'https://backend-arg-programa-production.up.railway.app',
+  URL: 'https://backend-arg-programa-production.up.railway.app/',
 };
